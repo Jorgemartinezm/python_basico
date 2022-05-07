26aa5e118571af42cb7cc0fd9cd9a414e2a0efc1
Jorge Martínez <jjorgemartinezmadrigal@gmail.com>
2022-05-07 18:34:03 +0200

    transformamonedas mejorado

--- transformamonedas1.py
+++ transformamonedas1.py
@@ -1,11 +1,48 @@
-# dolar = input('''¡¡¡Bienvenido al transformamonedas!!!
-#       Ahora puede pasar de euro a dólares
-#       Elija la cantidad deseada: ''')
-#
-# dolar = float(dolar)
-# euro = 0.94
-# resultado = dolar / euro
-# resultado = round(resultado, 2)
-# resultado = str(resultado)
-#
-# print('Tienes ', resultado, 'euros')
+menu = """
+Bienvenido al conversor de monedas
+
+1 - Euros
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    dolar = input('Elija la cantidad deseada: ')
+    dolar = float(dolar)
+    euro = 0.94
+    resultado = dolar / euro
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print('Tienes ', resultado, 'euros')
+
+elif opcion == '2':
+    dolar = input('Elija la cantidad deseada: ')
+    dolar = float(dolar)
+    pargentino = 65
+    resultado = dolar / pargentino
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print('Tienes ', resultado, 'pesos argentinos')
+    
+elif opcion == '3':
+    dolar = input('Elija la cantidad deseada: ')
+    dolar = float(dolar)
+    pmexicano = 24
+    resultado = dolar / pmexicano
+    resultado = round(resultado, 2)
+    resultado = str(resultado)
+    print('Tienes ', resultado, 'pesos mexicanos')
+
+else:
+      print("Elige una opción válida")
+
+
+
+
+
+
+
+
