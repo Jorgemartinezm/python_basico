f7236bfb584ffee2fcee88936698aa4996ef8204
Jorge Martínez <jjorgemartinezmadrigal@gmail.com>
2022-05-07 18:16:34 +0200

    comentando todo el transformamonedas para empezar con lo bueno

--- transformamonedas1.py
+++ transformamonedas1.py
@@ -1,11 +1,11 @@
-dolar = input('''¡¡¡Bienvenido al transformamonedas!!!
-      Ahora puede pasar de euro a dólares
-      Elija la cantidad deseada: ''')
-
-dolar = float(dolar)
-euro = 0.94
-resultado = dolar / euro
-resultado = round(resultado, 2)
-resultado = str(resultado)
-
-print('Tienes ', resultado, 'euros')
+# dolar = input('''¡¡¡Bienvenido al transformamonedas!!!
+#       Ahora puede pasar de euro a dólares
+#       Elija la cantidad deseada: ''')
+#
+# dolar = float(dolar)
+# euro = 0.94
+# resultado = dolar / euro
+# resultado = round(resultado, 2)
+# resultado = str(resultado)
+#
+# print('Tienes ', resultado, 'euros')
