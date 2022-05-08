ab471789a4571511a1dc411ea450cbf2b7db397e
Jorge Martínez <jjorgemartinezmadrigal@gmail.com>
2022-05-08 10:03:28 +0200

    ciclo for

---
+++ ciclo_for.py
@@ -0,0 +1,19 @@
+# def run():
+#     NUMERO_MAXIMO = 1000
+#     contador = 1
+#     while contador <= NUMERO_MAXIMO:
+#         print('Número: ' + str(contador))
+#         contador += 1
+        
+
+# if __name__ == '__main__':
+#     run()
+
+for contador in range(1000):
+    print(contador)
+    
+# a = list(range(1000))
+# print(a)
+
+for i in range(10):
+    print(11 * i)
