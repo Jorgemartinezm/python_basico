cc464a0cbe22b15bf0a1bae6291002b86f2a2129
Jorge Martínez <jjorgemartinezmadrigal@gmail.com>
2022-05-08 11:28:00 +0200

    break and continue

---
+++ break_continue.py
@@ -0,0 +1,31 @@
+from tkinter import N
+
+
+def run():
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+    
+    # for i in range(10000):
+    #     print(i)
+    #     if i == 5678:
+    #         break
+    
+    # texto = input('Escribe un texto: ')
+    # for letra in texto:
+    #     if letra == 'o':
+    #         break
+    #     print(letra)
+    
+    
+    frase = input('Escribe una frase: ')
+    for letra in frase:
+        letra = str(letra)
+        if letra == "a":
+            continue
+        print(letra)
+
+if __name__ == '__main__':
+    run()
+    
