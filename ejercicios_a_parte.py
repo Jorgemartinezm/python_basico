d9ad52c9677972be5d9662f84131faf22e9b98ad
Jorge Martínez <jjorgemartinezmadrigal@gmail.com>
2022-05-14 19:31:58 +0200

    cosas hechas a parte

---
+++ ejercicios_a_parte.py
@@ -0,0 +1,91 @@
+# def run():
+#     palabra_frase = str(input('Escribe algo: '))
+#     for i in range(10):
+#         print(palabra_frase)
+    
+# if __name__ == '__main__':
+#     run()
+
+
+
+# def run():
+    
+#     edad = 1
+#     EDAD = 14
+
+#     while edad != EDAD:
+#         print('Tenías ' + str(edad) + ' años')
+#         edad = edad + 1
+
+# if __name__ == '__main__':
+#     run()
+
+
+
+# def run():
+    
+#     # numero_base = 1
+      
+#     # numero_impar = 0
+
+#     # if (numero_impar % 2) == 0:
+#     #     numero_impar = 2
+#     # else:
+#     #     numero_impar = 1
+    
+#     # numero_algo = int(input("Indique un número: "))
+#     # print('El número: 1 es impar; ')    
+#     # while numero_algo != numero_base:
+#     #     numero_base = numero_impar + numero_base        
+#     #     if numero_algo <= numero_base:
+#     #         break
+#     #     print('El número: ' + str(numero_base) + ' es impar; ')
+   
+#    n = int(input('Escribe un numero: '))
+#    for i in range(1, 1+n, 2):
+#        print(str(i), end = ', ')
+   
+# if __name__ == '__main__':
+#     run()
+
+
+
+# def run():
+#     n = int(input('Escribe un número: '))
+#     for i in range(n):
+#         n = n -1
+#         print(n, end=', ')
+
+# if __name__ == '__main__':
+#     run()
+
+
+
+# def run():
+#     n = int(input('Elige el número de estrellas: '))
+#     for i in range(n):
+#         for j in range(i+1):
+#             print('*', end='')
+#         print('')
+    
+
+# if __name__ == '__main__':
+#     run()
+
+
+
+# def run():
+#     MAX_NUMS = 10
+#     nd = 1
+#     p = 1
+#     for i in range(1, 11):
+#         for j in range(MAX_NUMS):
+#             print(str(n) + ' por ' + str(p) + ' da: ' + str(n *p))
+#             p = p +1
+#         n = n +1
+#         p = 1    
+#         print('')
+        
+
+# if __name__ == '__main__':
+#     run()
